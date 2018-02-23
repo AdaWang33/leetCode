403e3af37a45510845e0b5b22a672e3f7e27403f
yaofei wang <yaofeiwang@yaofeideMacBook-Pro.local>
2018-02-22 21:55:08 -0500

    第一题完成

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -7,28 +7,31 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        """
         l = len(nums)
         # 用列表生成器来做，非常省力
         return [[x, y] for x in xrange(l) for y in xrange(x,l) if nums[x] + nums[y] == target and x != y][0]
         
-        """
-        # 不知道问题在哪里总之错了，另外下面的答案也没看懂
+
+        # 知道错在哪里了，返回的是值而不是序号，但答案依然没看懂
         l = len(nums)
-        a = [[x,(target-x)] for x in xrange(l) if (target-x) in nums][0]
+        a = [[nums[x],(target-nums[x])] for x in range(l) if (target-nums[x]) in nums and (target-nums[x]) != nums[x]][0]
         return a
         """
+
         
-        """
+
         # 此为答案
         d={}
         for i,num in enumerate(nums):
             if target-num in d:
                 return d[target-num], i
             d[num]=i
-        """
+    
         
         
         
         
         
+                
         
