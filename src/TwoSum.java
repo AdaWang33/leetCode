b61d30f11ed7f3a0c3b93dd7b86ec3b8b3e25f6d
AdaWang33 <vivianqqy@gmail.com>
2019-06-18 14:11:06 +0800

    formatted

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -5,14 +5,14 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         // create hashmap, mapping from list element to its index
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
 
-        for(int i = 0; i< nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             // if list contains complement and is not the same element
-            if(map.containsKey(complement) && i != map.get(complement)) return(new int[] {i, map.get(complement)});
+            if (map.containsKey(complement) && i != map.get(complement)) return (new int[]{i, map.get(complement)});
         }
         throw new IllegalArgumentException("No sum solution");
     }
