461e5a7bf04d0a0bf6ca84ddb3499fa727f52cf8
AdaWang33 <vivianqqy@gmail.com>
2019-06-27 23:34:15 +0800

    to do

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
 
 public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        s = preprocess(s);
+        int max = 0;
+        String maxString = "";
+        for (int i = 0; i < s.length(); i++) {
+            int rl = 0;
+            if (i - rl > 0 && i + rl < s.length() && s[i - rl] == s[i + rl]) {
+                rl++;
+            }
+            if (2 * rl - 1 > max) {
+                max = 2 * rl - 1;
+                maxString = s.substring(i - rl + 1, i + rl );
+            }
+        }
+        return maxString;
+    }
+
+    public String preprocess(String s) {
+        String ps = "#";
+        for (int i = 0; i < s.length(); i++) {
+            ps += s.charAt(i) + "#";
+        }
+        return ps;
+    }
 }
