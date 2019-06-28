e9f1ec7954bbd0ae2fd63b3a361205ec3727c1bd
AdaWang33 <vivianqqy@gmail.com>
2019-06-28 19:51:19 +0800

    较为朴素的算法

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -6,7 +6,8 @@ public class LongestPalindromicSubstring {
         String maxString = "";
         for (int i = 0; i < s.length(); i++) {
             int rl = 0;
-            if (i - rl > 0 && i + rl < s.length() && s[i - rl] == s[i + rl]) {
+            // while 和 if不要搞错
+            while (i - rl > 0 && i + rl < s.length() && s.charAt(i - rl) == s.charAt(i + rl)) {
                 rl++;
             }
             if (2 * rl - 1 > max) {
@@ -14,7 +15,8 @@ public class LongestPalindromicSubstring {
                 maxString = s.substring(i - rl + 1, i + rl );
             }
         }
-        return maxString;
+        String result = maxString.replaceAll("[#]","");
+        return result;
     }
 
     public String preprocess(String s) {
@@ -25,3 +27,8 @@ public class LongestPalindromicSubstring {
         return ps;
     }
 }
+
+
+
+
+
