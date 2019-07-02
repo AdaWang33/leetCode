09c2ddc416c8aefdd7047d0f6a4a6db5b345a72f
AdaWang33 <vivianqqy@gmail.com>
2019-07-02 20:50:02 +0800

    直接用俩array，更快了

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -3,24 +3,25 @@ import java.util.HashMap;
 public class RomanToInteger {
     public int romanToInt(String s) {
         int ans = 0;
-        HashMap<Character, Integer> lookup = new HashMap<Character, Integer>();
-        lookup.put('I', 1);
-        lookup.put('V', 5);
-        lookup.put('X', 10);
-        lookup.put('L', 50);
-        lookup.put('C', 100);
-        lookup.put('D', 500);
-        lookup.put('M', 1000);
+//        HashMap<Character, Integer> lookup = new HashMap<Character, Integer>();
+//        lookup.put('I', 1);
+//        lookup.put('V', 5);
+//        lookup.put('X', 10);
+//        lookup.put('L', 50);
+//        lookup.put('C', 100);
+//        lookup.put('D', 500);
+//        lookup.put('M', 1000);
 
-        String idx = new String("IVXLCDM");
+        String chara = new String("IVXLCDM");
+        int[] number = new int[]{1,5,10,50,100,500,1000};
         for (int i = 0; i < s.length() - 1; i++) {
             char c = s.charAt(i);
             char cr = s.charAt(i+1);
-            if (idx.indexOf(c) >= idx.indexOf(cr)) {
-                ans += lookup.get(c);
-            } else ans += -lookup.get(c);
+            if (chara.indexOf(c) >= chara.indexOf(cr)) {
+                ans += number[chara.indexOf(c)];
+            } else ans -= number[chara.indexOf(c)];;
         }
-        ans += lookup.get(s.charAt(s.length() - 1));
+        ans += number[chara.indexOf(s.charAt(s.length() - 1))];
     return ans;
     }
 }
