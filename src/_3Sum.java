6fbc7eed1b0a67402f59add45506d911c9c030d0
AdaWang33 <vivianqqy@gmail.com>
2019-07-03 19:49:01 +0800

    化三为二

---
+++ src/_3Sum.java
@@ -0,0 +1,56 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+/**
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate triplets.
+ * <p>
+ * Example:
+ * <p>
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * <p>
+ * A solution set is:
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
+
+public class _3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // doesn't need to be a fix-sized object
+        //List<List<Integer>> result = new ArrayList<Integer>();
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        if (len == 0 || len < 3) return result;
+        for (int i = 0; i < len - 2; i++) {
+            if (i >0 && nums[i] == nums[i - 1]) continue;
+            int remains = -nums[i];
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == remains) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < remains) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
