6e6fd986bd404726916c68cf11325d132f1f08d0
AdaWang33 <vivianqqy@gmail.com>
2019-07-06 13:55:40 +0800

    Linked List：可构造相同值的node，避免pointer的混乱情况

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,44 @@
+/**
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode pseudoHead = new ListNode(0);
+        ListNode current = pseudoHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                current.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        if (l1 == null) {
+            current.next = l2;
+        }
+        if (l2 == null) {
+            current.next = l1;
+        }
+        return pseudoHead.next;
+    }
+
+}
