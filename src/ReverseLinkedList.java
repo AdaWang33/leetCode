c5c388eba36fb93d4a2b156d70b608e023a6c3db
AdaWang33 <vivianqqy@gmail.com>
2019-07-15 17:23:47 +0800

    Linked List: 反转链表，iteration法，待补充recursion法

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,19 @@
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        // iteratively
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        head.next = null;
+        return prev;
+    }
+}
