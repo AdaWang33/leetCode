4d82c98a06f98b63b24dd6fb2f8f3df51b1187ef
AdaWang33 <vivianqqy@gmail.com>
2019-07-16 14:55:16 +0800

    Linked List: 利用stack来达到不反转链表本身同时可以倒序相加的目的

--- src/AddTwoNumbersII.java
+++ src/AddTwoNumbersII.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
  * <p>
@@ -14,49 +16,38 @@
 
 public class AddTwoNumbersII {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // 先得到两个的位数，再逐位相加，用prev, cur,和jin来表示
-        int len1 = 0, len2 = 0;
-        ListNode tmp1 = l1, tmp2 = l2;
-        while (tmp1 != null) {
-            len1++;
-            tmp1 = tmp1.next;
-        }
-        while (tmp2 != null) {
-            len2++;
-            tmp2 = tmp2.next;
-        }
-        if (len1 <= len2) {
-            tmp1 = l1;
-            l1 = l2;
-            l2 = tmp1;
-            int tmp = 0;
-            tmp = len1;
-            len1 = len2;
-            len2 = tmp;
-        }
+        // 先得到两个的位数，再逐位相加，用prev, cur,和jin来表示(实际操作后发现不可行，多项进位不好操作)
+        // 可利用stack来达成不改变Linked List本身，但是又倒序的目的
 
-        // 开始逐位相加
-        ListNode dummy = new ListNode(0);
-        ListNode prev = dummy;
-        while (len1 != len2) {
-            prev.next = l1;
-            prev = prev.next;
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
             l1 = l1.next;
-            len1--;
         }
-
-        while (len1 > 0) {
-            int sum = l1.val+l2.val;
-            int jin = sum/10;
-            int rem = sum%10;
-            ListNode cur = new ListNode(rem);
-            prev.val += jin;
-            prev.next = cur;
-            prev = prev.next;
-            l1 = l1.next;
+        while (l2 != null) {
+            stack2.push(l2.val);
             l2 = l2.next;
-            len1--;
         }
-        return dummy.next;
+
+        int carry = 0;
+        ListNode dummy = null;
+        ListNode cur = dummy;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int sum = 0;
+            sum += carry;
+            if (!stack1.isEmpty()) {
+                sum += stack1.pop();
+            }
+            if (!stack2.isEmpty()) {
+                sum += stack2.pop();
+            }
+            int digits = sum % 10;
+            carry = sum / 10;
+            ListNode head = new ListNode(digits);
+            head.next = cur;
+            cur = head;
+        }
+        return cur;
     }
 }
