000eac70ae17a4f7a267fe3f102be500dea53ac9
AdaWang33 <vivianqqy@gmail.com>
2019-07-16 23:12:50 +0800

    Linked List: 勉勉强强算是写出来了recursion,收尾条件和retuen重复时可以省略

--- src/FlattenAMultilevelDoublyLinkedList.java
+++ src/FlattenAMultilevelDoublyLinkedList.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node() {
+    }
+
+    public Node(int _val, Node _prev, Node _next, Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+}
 
 public class FlattenAMultilevelDoublyLinkedList {
+    public Node flatten(Node head) {
+        if (head == null) return null;
+
+        Node cur = head;
+        while (cur != null) {
+            if (cur.child == null) cur = cur.next;
+            else {
+                Node nex = cur.next;
+                Node child = flatten(cur.child);
+                cur.next = child;
+                child.prev = cur;
+                cur.child = null;
+                Node end = child;
+                while (end.next != null) {
+                    end = end.next;
+                }
+                end.next = nex;
+                if (nex != null) nex.prev = end;
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
 }
