b0b5698b2f279c638a88a7248f4e6bbd8e35ebb2
AdaWang33 <vivianqqy@gmail.com>
2019-07-20 00:02:22 +0800

    Tree: 经典recursion的用法

---
+++ src/UniqueBinarySearchTrees.java
@@ -0,0 +1,111 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ * <p>
+ * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 3
+ * Output:
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ * <p>
+ * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 3
+ * Output:
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ */
+
+/**
+ * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+ *
+ * Example:
+ *
+ * Input: 3
+ * Output:
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueBinarySearchTrees {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<TreeNode>();
+
+        return createTree(1, n);
+    }
+
+    public List<TreeNode> createTree(int l, int r) {
+        if (l > r) {
+            List<TreeNode> line = new ArrayList<TreeNode>();
+            line.add(null);
+            return line;
+        }
+
+        List<TreeNode> ans = new ArrayList<>();
+        for (int i = l; i <= r; i++) {
+            List<TreeNode> lans = createTree(l,i-1);
+            List<TreeNode> rans = createTree(i+1,r);
+            for(int j = 0;j<lans.size();j++){
+                for(int k = 0;k<rans.size();k++){
+                    TreeNode cur = new TreeNode(i);
+                    cur.left = lans.get(j);
+                    cur.right = rans.get(k);
+                    ans.add(cur);
+                }
+            }
+        }
+        return ans;
+    }
+}
