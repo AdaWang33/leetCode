e87bf04c04943175b80e93164c2207d4bc685868
AdaWang33 <vivianqqy@gmail.com>
2019-07-21 21:19:48 +0800

    Tree: 左右非null才能选min，不然要选max

---
+++ src/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.right != null) return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        else return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+
+    }
+}
