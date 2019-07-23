b5d9426ffa9d69dcd92bb040ec1d88413bbfd530
AdaWang33 <vivianqqy@gmail.com>
2019-07-23 11:51:32 +0800

    Tree: 和前一题的答案用一个竟然通过了……

---
+++ src/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,27 @@
+import java.util.ArrayDeque;
+
+public class PopulatingNextRightPointersInEachNodeII {
+    public Node connect(Node root) {
+        if (root == null) return root;
+
+        Node cur = root;
+        ArrayDeque<Node> deque = new ArrayDeque<>();
+        deque.addLast(cur);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            ArrayDeque<Node> newDeque = new ArrayDeque<>();
+            for (int i = 0; i < size; i++) {
+                cur = deque.removeFirst();
+                if (!deque.isEmpty()) {
+                    cur.next = deque.peek();
+                } else {
+                    cur.next = null;
+                }
+                if(cur.left!=null) newDeque.addLast(cur.left);
+                if(cur.right!=null) newDeque.addLast(cur.right);
+            }
+            deque = newDeque;
+        }
+        return root;
+    }
+}
