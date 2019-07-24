cfd5c1eb53ba04db673dd04ee810c05d7795b308
AdaWang33 <vivianqqy@gmail.com>
2019-07-24 23:33:06 +0800

    Tree: 从这个点开始算，算完切另一个点开始，清新

--- src/PathSumIII.java
+++ src/PathSumIII.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class PathSumIII {
+    int cnt = 0;
+
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+        beginsAt(root, sum);
+        return cnt;
+    }
+
+    public void beginsAt(TreeNode root, int sum) {
+        if (root == null) return;
+        findPath(root, sum);
+        beginsAt(root.left, sum);
+        beginsAt(root.right, sum);
+    }
+
+    public void findPath(TreeNode root, int sum) {
+        if (root == null) return;
+        sum -= root.val;
+        if (sum == 0) cnt++;
+        findPath(root.left, sum);
+        findPath(root.right, sum);
+    }
 }
