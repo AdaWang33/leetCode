9f1b178d6e54448548c1c076f67df489d5ae8c7c
AdaWang33 <vivianqqy@gmail.com>
2019-07-24 22:31:15 +0800

    Tree: 慢一点return，考虑自己，左子树和右子树

---
+++ src/SumOfLeftLeaves.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+/**
+ * Find the sum of all left leaves in a given binary tree.
+ * <p>
+ * Example:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+ */
+
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int tmp = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) tmp = root.left.val;
+        return tmp + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
