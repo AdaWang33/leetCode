24fef45d655a83c7830173ece2905c2fcde50410
AdaWang33 <vivianqqy@gmail.com>
2019-07-26 00:12:34 +0800

    Tree: dfs, 对抗&帮助

--- src/BinaryTreeTilt.java
+++ src/BinaryTreeTilt.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class BinaryTreeTilt {
+    int ans = 0;
+
+    public int findTilt(TreeNode root) {
+        if (root == null) return 0;
+        findAbs(root);
+        return ans;
+    }
+
+    public int findAbs(TreeNode root) {
+        if (root == null) return 0;
+        int left = findAbs(root.left);
+        int right = findAbs(root.right);
+        ans += Math.abs(left - right);
+        return root.val + left + right;
+    }
 }
+
