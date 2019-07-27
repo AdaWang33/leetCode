9890ed14525250da40aae288b9ac01d4637521e8
AdaWang33 <vivianqqy@gmail.com>
2019-07-27 15:06:59 +0800

    Tree: quick union的基础上，find：找根，union：改根

---
+++ src/RedundantConnection.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RedundantConnection {
+    int[] parent = new int[2001];
+
+    public int[] findRedundantConnection(int[][] edges) {
+        for (int i = 0; i < parent.length; i++) parent[i] = i;
+
+        // union op for each edge
+        for (int[] edge : edges) {
+            int left = edge[0];
+            int right = edge[1];
+            if (find(left) != find(right)) {
+                parent[find(left)] = find(right);
+            } else {
+                return edge;
+            }
+        }
+        return new int[2];
+    }
+
+    public int find(int idx) {
+        if (idx != parent[idx]) {
+            parent[idx] = find(parent[idx]);
+        }
+        return parent[idx];
+    }
+}
