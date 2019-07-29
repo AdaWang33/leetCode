c858c25cb420005d5ff0d60e3b4f19baefa38776
AdaWang33 <vivianqqy@gmail.com>
2019-07-29 17:14:41 +0800

    Tree: BST可以跳来跳去（eg：delete）的特性不要忘，不要先用binary tree的普遍做法

--- src/RangeSumOfBST.java
+++ src/RangeSumOfBST.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+/**
+ * int sum = 0;
+ * <p>
+ * public int rangeSumBST(TreeNode root, int L, int R) {
+ * addUp(root, L, R);
+ * return sum;
+ * }
+ * <p>
+ * public void addUp(TreeNode root, int L, int R) {
+ * if (root == null) return;
+ * addUp(root.left, L, R);
+ * if (root.val >= L && root.val <= R) sum += root.val;
+ * addUp(root.right, L, R);
+ * }
+ */
 
 public class RangeSumOfBST {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        // 要利用BST可以跳来跳去的特性，不要用BT的普适做法(in-order traversal)
+        if (root == null) return 0;
+        if (root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        } else if (root.val > R) {
+            return rangeSumBST(root.left, L, R);
+        } else {
+            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+        }
+    }
 }
