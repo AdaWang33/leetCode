a33640b1b8bbfa0a94b09022d93dbb49b18db6db
AdaWang33 <vivianqqy@gmail.com>
2019-07-31 15:09:37 +0800

    Tree: root到每一个leaf的类型，if（left right都等于0，）if（left==

---
+++ src/SumOfRootToLeafBinaryNumbers.java
@@ -0,0 +1,19 @@
+public class SumOfRootToLeafBinaryNumbers {
+    int Sum = 0;
+
+    public int sumRootToLeaf(TreeNode root) {
+        if (root == null) return Sum;
+        findSum(root, 0);
+        return Sum;
+    }
+
+    public void findSum(TreeNode root, int sum) {
+        sum = 2 * sum + root.val;
+        if (root.left == null && root.right == null) {
+            Sum += sum;
+            return;
+        }
+        if (root.left != null) findSum(root.left, sum);
+        if (root.right != null) findSum(root.right, sum);
+    }
+}
