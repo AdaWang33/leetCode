f1f767ef99cedcd2bc1b75b89a3f12388bbc0e94
AdaWang33 <vivianqqy@gmail.com>
2019-08-08 20:47:17 +0800

    Stack: 嘻嘻，学到了next greater从后往前stk

--- src/DailyTemperatures.java
+++ src/DailyTemperatures.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] T) {
+        int[] ans = new int[T.length];
+        Stack<Integer> stk = new Stack<>(); // 存的是idx
+
+        for (int i = T.length - 1; i >= 0; i--) {
+            while (!stk.isEmpty() && T[stk.peek()] <= T[i]) {
+                stk.pop();
+            }
+            if (stk.isEmpty()) ans[i] = 0;
+            else ans[i] = stk.peek() - i;
+            stk.push(i);
+        }
+
+        return ans;
+
+    }
 }
