1bd80b88de70bc8f073e96f09b9b78c1470ed5e5
AdaWang33 <vivianqqy@gmail.com>
2019-08-09 12:27:13 +0800

    Stack: String.valueOf()可以用来初始化stack

---
+++ src/BackspaceStringCompare.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class BackspaceStringCompare {
+    public boolean backspaceCompare(String S, String T) {
+        String s = processedString(S);
+        String t = processedString(T);
+        return s.equals(t);
+
+    }
+
+    public String processedString(String str) {
+        Stack<Character> stk = new Stack<>();
+        for (Character c : str.toCharArray()) {
+            if (c != '#') stk.push(c);
+            else {
+                if (!stk.isEmpty()) stk.pop();
+            }
+        }
+        return String.valueOf(stk);
+    }
+}
