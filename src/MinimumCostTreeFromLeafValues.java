0a4f6a51a24faaa0912d9b8d7a1c78f088f82b0a
AdaWang33 <vivianqqy@gmail.com>
2019-08-10 23:11:57 +0800

    Stack: 维护递减的单向栈，从而保证小的被大的pop调

---
+++ src/MinimumCostTreeFromLeafValues.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class MinimumCostTreeFromLeafValues {
+    public int mctFromLeafValues(int[] arr) {
+        int ans = 0;
+        Stack<Integer> stk = new Stack<>();
+        stk.push(Integer.MAX_VALUE);
+
+        for (int num : arr) {
+            while (!stk.isEmpty() && stk.peek() <= num) {
+                ans += stk.pop() * (Math.min(stk.peek(), num));
+            }
+            stk.push(num);
+        }
+
+        if (stk.size() >= 2) {
+            ans += stk.pop() * stk.peek();
+        }
+        return ans;
+    }
+}
