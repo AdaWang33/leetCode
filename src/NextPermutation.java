fd3caefe2af89b02bc9c1a9513e6bcd68e7f868e
AdaWang33 <vivianqqy@gmail.com>
2019-10-29 13:46:37 -0400

    int permutation: desc order and first one that breaks the order, swap, reverse

---
+++ src/NextPermutation.java
@@ -0,0 +1,25 @@
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        if (nums.length == 0 || nums.length == 1) return;
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--; // find the one that breaks desc order
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+        }
+        reverOrder(nums, i + 1, nums.length - 1);
+
+    }
+
+    public void swap(int[] nums, int p, int q) {
+        int tmp;
+        tmp = nums[p];
+        nums[p] = nums[q];
+        nums[q] = tmp;
+    }
+
+    public void reverOrder(int[] nums, int p, int q) {
+        while (p <= q) swap(nums, p++, q--);
+    }
+}
