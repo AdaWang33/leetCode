81dfd1c12f49ccc2758e0987ab3a0510f16d8313
AdaWang33 <vivianqqy@gmail.com>
2019-11-08 00:34:21 -0500

    提供一个更简洁的做法

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -2,19 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
-    List<String> resList = new ArrayList<>();
+//    List<String> resList = new ArrayList<>();
+//
+//    public List<String> generateParenthesis(int n) {
+//        buildParenthesis("", 0, 0, n);
+//        return resList;
+//    }
+//
+//    public void buildParenthesis(String str, int open, int close, int n) {
+//        if (str.length() == 2 * n) {
+//            resList.add(str);
+//            return;
+//        }
+//        if (open < n) buildParenthesis(str + "(", open + 1, close, n);
+//        if (close < open) buildParenthesis(str + ")", open, close + 1, n);
+//    }
+
+    List<String> res = new ArrayList<>();
 
     public List<String> generateParenthesis(int n) {
-        buildParenthesis("", 0, 0, n);
-        return resList;
+        helper("", 0, n);
+        return res;
     }
 
-    public void buildParenthesis(String str, int open, int close, int n) {
+    public void helper(String str, int cnt, int n) {
         if (str.length() == 2 * n) {
-            resList.add(str);
+            if (cnt == 0) res.add(str);
             return;
         }
-        if (open < n) buildParenthesis(str + "(", open + 1, close, n);
-        if (close < open) buildParenthesis(str + ")", open, close + 1, n);
+        if (cnt < 0) return;
+        helper(str + "(", cnt + 1, n);
+        helper(str + ")", cnt - 1, n);
+
     }
 }
