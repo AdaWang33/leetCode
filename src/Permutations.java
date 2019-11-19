917fec1060c969a2eb66cc195a1ed10e80dbfddb
AdaWang33 <vivianqqy@gmail.com>
2019-11-19 13:18:08 -0500

    subset, permutation, combination sum

--- src/Permutations.java
+++ src/Permutations.java
@@ -1,25 +1,41 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Given a collection of distinct integers, return all possible permutations.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input: [1,2,3]
  * Output:
  * [
- *   [1,2,3],
- *   [1,3,2],
- *   [2,1,3],
- *   [2,3,1],
- *   [3,1,2],
- *   [3,2,1]
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
  * ]
  */
 
 public class Permutations {
-    public List<List<Integer>> permute(int[] nums) {
-        int n = nums.length;
+    List<List<Integer>> res = new ArrayList<>();
 
+    public List<List<Integer>> permute(int[] nums) {
+        generatePermutations(nums, new ArrayList<>());
+        return res;
+    }
+
+    public void generatePermutations(int[] nums, List<Integer> tmp) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int num : nums) {
+            if (tmp.contains(num)) continue;
+            tmp.add(num);
+            generatePermutations(nums, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
     }
 }
