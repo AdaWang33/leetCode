bb1f2969c49ae7fc414844038ca8c2adeb1c9a98
AdaWang33 <vivianqqy@gmail.com>
2019-11-21 16:41:50 -0500

    dynamic programming: 加入或者自立门户，外面维护一个max来保证最大或最长

---
+++ src/MaximumSubarray.java
@@ -0,0 +1,28 @@
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Follow up:
+ * <p>
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        // dynamic programming: 加入或者自立门派，外面维护一个max
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
