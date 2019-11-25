56328cb12f476a540bffff540f442cde135d5e04
AdaWang33 <vivianqqy@gmail.com>
2019-11-24 22:48:34 -0500

    typical dynamic programming with table[i][j]

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -8,6 +8,7 @@
 
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
+        // dynamic programming
         int[][] ans = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
