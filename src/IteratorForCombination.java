013dfbc5c560067a30db9cb8460e13d6d943ebcf
AdaWang33 <vivianqqy@gmail.com>
2019-12-15 15:06:13 -0500

    backtrack

--- src/IteratorForCombination.java
+++ src/IteratorForCombination.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IteratorForCombination {
+    List<String> res;
+    int nextIndex = 0;
+
+    public IteratorForCombination(String characters, int combinationLength) {
+        res = new ArrayList<>();
+        generateCombination(characters, 0, combinationLength, new StringBuilder());
+    }
+
+    public void generateCombination(String characters, int index, int combinationLength, StringBuilder sb) {
+        if (sb.length() == combinationLength) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (index == characters.length()) return;
+
+        for (int i = index; i < characters.length(); i++) {
+            sb.append(characters.charAt(i));
+            generateCombination(characters, i+1, combinationLength, sb);
+            sb.setLength(sb.length() - 1);
+        }
+    }
+
+    public String next() {
+        String ans = res.get(nextIndex);
+        nextIndex++;
+        return ans;
+    }
+
+    public boolean hasNext() {
+        if (nextIndex < res.size()) return true;
+        else return false;
+    }
 }
