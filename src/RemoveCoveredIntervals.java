ed3d0bfa558b1084cea9edb7530e72646b6b7b67
AdaWang33 <vivianqqy@gmail.com>
2019-12-15 15:06:49 -0500

    sorted array, two for loop to

--- src/RemoveCoveredIntervals.java
+++ src/RemoveCoveredIntervals.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class RemoveCoveredIntervals {
+    public int removeCoveredIntervals(int[][] intervals) {
+        int res = intervals.length;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+            for (int j = i + 1; j < intervals.length; j++) {
+                if (intervals[i][1] == Integer.MAX_VALUE) continue;
+                if (intervals[i][1] >= intervals[j][1]) {
+                    res--;
+                    intervals[j][1] = Integer.MAX_VALUE;
+                } else continue;
+            }
+        }
+        return res;
+    }
 }
