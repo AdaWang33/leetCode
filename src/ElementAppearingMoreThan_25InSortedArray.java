c5d4ed8c0716f0894066d24bc387d1eae9f70896
AdaWang33 <vivianqqy@gmail.com>
2019-12-15 15:05:48 -0500

    count works

--- src/ElementAppearingMoreThan_25InSortedArray.java
+++ src/ElementAppearingMoreThan_25InSortedArray.java
@@ -1,2 +1,19 @@
+import java.util.HashMap;
+
 public class ElementAppearingMoreThan_25InSortedArray {
+    public int findSpecialInteger(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int bigKey = arr[0];
+        int bigVal = 0;
+
+        for (int ele : arr) {
+            int newVal = map.getOrDefault(ele, 0) + 1;
+            map.put(ele, newVal);
+            if(newVal>bigVal) {
+                bigKey = ele;
+                bigVal = newVal;
+            }
+        }
+        return bigKey;
+    }
 }
