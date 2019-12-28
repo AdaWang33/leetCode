77125efcdd9e2c5835b211efa7c45a7abfe80315
AdaWang33 <vivianqqy@gmail.com>
2019-12-28 11:46:38 -0500

    reverse order

--- src/ReplaceElementsWithGreatestElementOnRightSide.java
+++ src/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class ReplaceElementsWithGreatestElementOnRightSide {
+    public int[] replaceElements(int[] arr) {
+        int maxRight = arr[arr.length-1];
+        int curMax = Integer.MIN_VALUE;
+        for(int i =arr.length-2;i>=0;i--){
+            curMax = Math.max(arr[i],maxRight);
+            arr[i] = maxRight;
+            if(curMax>maxRight){
+                maxRight = curMax;
+            }
+        }
+        arr[arr.length-1] = -1;
+        return arr;
+    }
+
 }
