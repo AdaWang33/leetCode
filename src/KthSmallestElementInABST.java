a1bdeffdd4a5c189860e9b829636f975a6da10d9
AdaWang33 <vivianqqy@gmail.com>
2020-02-03 18:08:55 -0500

    inorder traversal要记住, stack: when to load, when to pop, use cur

--- src/KthSmallestElementInABST.java
+++ src/KthSmallestElementInABST.java
@@ -1,25 +1,46 @@
 import java.util.Stack;
 
 public class KthSmallestElementInABST {
+//    public int kthSmallest(TreeNode root, int k) {
+//        if (root == null) return 0;
+//
+//        int ans = 0;
+//        Stack<TreeNode> stk = new Stack<>();
+//
+//        while (root != null || !stk.isEmpty()) {
+//            if (root != null) {
+//                stk.add(root);
+//                root = root.left;
+//            } else {
+//                root = stk.pop();
+//                if (--k == 0) {
+//                    ans =  root.val;
+//                    break;
+//                }
+//                root = root.right;
+//            }
+//        }
+//        return ans;
+//    }
     public int kthSmallest(TreeNode root, int k) {
-        if (root == null) return 0;
+        // inorder traversal要记住, stack: when to load, when to pop, use cur
+        if(root==null) return 0;
 
-        int ans = 0;
+        int cnt = 0;
         Stack<TreeNode> stk = new Stack<>();
-
-        while (root != null || !stk.isEmpty()) {
-            if (root != null) {
-                stk.add(root);
-                root = root.left;
-            } else {
-                root = stk.pop();
-                if (--k == 0) {
-                    ans =  root.val;
-                    break;
-                }
-                root = root.right;
+        TreeNode cur = root;
+        while(cur!=null || !stk.isEmpty()){
+            if(cur!=null){
+                stk.push(cur);
+                cur = cur.left;
+            }else{
+                TreeNode node = stk.pop();
+                cnt++;
+                if(cnt==k) return node.val;
+                cur = node.right;
             }
         }
-        return ans;
+        return cur.val;
+
     }
 }
