4057ce66716753c8ea0fa1723144e414e93ff36e
AdaWang33 <vivianqqy@gmail.com>
2020-02-03 23:46:23 -0500

    hashmap

---
+++ src/VowelSpellchecker.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+/**
+ * Given a wordlist, we want to implement a spellchecker that converts a query word into a correct word.
+ *
+ * For a given query word, the spell checker handles two categories of spelling mistakes:
+ *
+ * Capitalization: If the query matches a word in the wordlist (case-insensitive), then the query word is returned with the same case as the case in the wordlist.
+ * Example: wordlist = ["yellow"], query = "YellOw": correct = "yellow"
+ * Example: wordlist = ["Yellow"], query = "yellow": correct = "Yellow"
+ * Example: wordlist = ["yellow"], query = "yellow": correct = "yellow"
+ * Vowel Errors: If after replacing the vowels ('a', 'e', 'i', 'o', 'u') of the query word with any vowel individually, it matches a word in the wordlist (case-insensitive), then the query word is returned with the same case as the match in the wordlist.
+ * Example: wordlist = ["YellOw"], query = "yollow": correct = "YellOw"
+ * Example: wordlist = ["YellOw"], query = "yeellow": correct = "" (no match)
+ * Example: wordlist = ["YellOw"], query = "yllw": correct = "" (no match)
+ * In addition, the spell checker operates under the following precedence rules:
+ *
+ * When the query exactly matches a word in the wordlist (case-sensitive), you should return the same word back.
+ * When the query matches a word up to capitlization, you should return the first such match in the wordlist.
+ * When the query matches a word up to vowel errors, you should return the first such match in the wordlist.
+ * If the query has no matches in the wordlist, you should return the empty string.
+ * Given some queries, return a list of words answer, where answer[i] is the correct word for query = queries[i].
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: wordlist = ["KiTe","kite","hare","Hare"], queries = ["kite","Kite","KiTe","Hare","HARE","Hear","hear","keti","keet","keto"]
+ * Output: ["kite","KiTe","KiTe","Hare","hare","","","KiTe","","KiTe"]
+ */
+
+public class VowelSpellchecker {
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        String[] res = new String[queries.length];
+        Set<String> words = new HashSet<>(Arrays.asList(wordlist));
+        Map<String, String> low = new HashMap<>();
+        Map<String, String> vowel = new HashMap<>();
+
+        for(String w:wordlist){
+            String lower = w.toLowerCase();
+            String devowel = lower.replaceAll("[aeiou]", "#");
+            low.putIfAbsent(lower, w);
+            vowel.putIfAbsent(devowel, w);
+        }
+
+        for(int i=0;i<queries.length;i++){
+            if(words.contains(queries[i])) {
+                res[i] = queries[i];
+                continue;
+            }
+            String lower = queries[i].toLowerCase();
+            String devowel = lower.replaceAll("[aeiou]", "#");
+            if(low.containsKey(lower)){
+                res[i] = low.get(lower);
+            }else if(vowel.containsKey(devowel)){
+                res[i] = vowel.get(devowel);
+            }else{
+                res[i] = "";
+            }
+        }
+        return res;
+    }
+}
