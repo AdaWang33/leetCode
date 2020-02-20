f1e1d00d285263f30dd4e3075bfa7afa88fc73ce
AdaWang33 <vivianqqy@gmail.com>
2020-02-20 17:00:28 -0500

    easy level

---
+++ src/GoatLatin.java
@@ -0,0 +1,41 @@
+public class GoatLatin {
+    Set<Character> set = new HashSet<>();
+    public String toGoatLatin(String S) {
+        if(S==null||S.length()==0) return S;
+        String res="";
+        set.add('a');
+        set.add('A');
+        set.add('e');
+        set.add('E');
+        set.add('i');
+        set.add('I');
+        set.add('o');
+        set.add('O');
+        set.add('u');
+        set.add('U');
+
+        String[] str = S.split("\\s+");
+        for(int i=0;i<str.length;i++){
+            res+=helper(str[i], i);
+            res+=" ";
+        }
+        res=res.substring(0,res.length()-1);
+        return res;
+    }
+
+    public String helper(String str, int index){
+        if(set.contains(str.charAt(0))){
+            str += "ma";
+        }else{
+            char c = str.charAt(0);
+            str = str.substring(1);
+            str+=c;
+            str+="ma";
+        }
+
+        while(index-->=0){
+            str+="a";
+        }
+        return str;
+    }
+}
