dc65b29245226989e36967f2f04f2a9c774fc136
AdaWang33 <vivianqqy@gmail.com>
2024-02-14 21:06:30 -0500

    something to keep in mind

---
+++ src/cheetSheet.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class cheetSheet {
+	public void ToMemorize() {
+		String[] array = new String[10];
+		int arraySize = array.length; // array --> length
+
+		String str = "Hello World";
+		int strLength = str.length(); // string --> length()
+
+		ArrayList<String> list = new ArrayList<String>();
+		int listSize = list.size(); // arrayList --> size()	
+	}
+}
