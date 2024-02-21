36d05e660cf19746f54eecc8622ae38378fe1b00
AdaWang33 <vivianqqy@gmail.com>
2024-02-20 21:29:49 -0500

    a simple loop can do the work, find the pattern is the key

--- src/validNumber.java
+++ src/validNumber.java
@@ -1,1 +1,34 @@
-te
+class Solution {
+    // https://leetcode.com/problems/valid-number/description/
+    public boolean isNumber(String s) {
+        //  
+        boolean digitSeen = false;
+        boolean dotSeen = false;
+        boolean eSeen = false;
+
+        for(int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if (c=='+' || c=='-') {
+                if(i>0 && s.charAt(i-1)!='e' && s.charAt(i-1)!='E') {
+                    return false;
+                }
+            } else if (c=='e' || c=='E') {
+                if(!digitSeen || eSeen){
+                    return false;
+                }
+                eSeen = true;
+                digitSeen = false;
+            } else if (c=='.') {
+                if (dotSeen || eSeen) {
+                    return false;
+                }
+                dotSeen = true;
+            } else if(Character.isDigit(c)) {
+                digitSeen = true;
+            } else {
+                return false;
+            }
+        }
+            return digitSeen;
+    }
+}
