7796175e7fe9c019dce962f1f3deedaef243c815
AdaWang33 <vivianqqy@gmail.com>
2024-03-19 21:23:20 -0400

    k sorted linkedNode --> divide and conquer --> merge sort

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -1,6 +1,7 @@
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+// 23. Merge k Sorted Lists
 /**
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
  * <p>
@@ -16,32 +17,82 @@ import java.util.Queue;
  */
 
 public class MergeKSortedLists {
+    // https://leetcode.com/problems/merge-k-sorted-lists/solutions/4677050/merge-sort-approach
     public ListNode mergeKLists(ListNode[] lists) {
-        // 利用PriorityQueue 来选出最小的一项
-        if (lists.length == 0 || lists == null) {
-            return null;
-        }
+        if(lists==null || lists.length==0) return null;
+        if(lists.length==1) return lists[0];
 
-        ListNode fakehead = new ListNode(0);
-        ListNode current = fakehead;
-
-        Queue<ListNode> minHeap = new PriorityQueue<ListNode>(lists.length, (node1, node2) -> Integer.compare(node1.val, node2.val));
-        for (ListNode listnode : lists) {
-            if (listnode != null) {
-                minHeap.add(listnode);
-            }
-        }
-
-        while (!minHeap.isEmpty()) {
-            ListNode min = minHeap.remove();
-            current.next = min;
-            current = current.next;
-
-            if (min.next != null) {
-                minHeap.add(min.next);
-            }
-        }
-
-        return fakehead.next;
+        return mergeSort(lists, 0, lists.length-1);
     }
+
+    private ListNode mergeSort(ListNode[] lists, int left, int right) {
+        if(left==right) return lists[left];
+
+        int mid = (left+right)/2;
+        ListNode leftNode = mergeSort(lists, left, mid);
+        ListNode rightNode = mergeSort(lists, mid+1, right);
+        return merge(leftNode, rightNode);
+    }
+
+    private ListNode merge(ListNode leftNode, ListNode rightNode){
+        if(leftNode ==null) return rightNode;
+        if(rightNode==null) return leftNode;
+        ListNode dummyHead = new ListNode(0);
+        ListNode curNode = dummyHead;
+
+        while(leftNode!=null && rightNode!=null) {
+            if(leftNode.val <= rightNode.val) {
+                curNode.next = leftNode;
+                leftNode = leftNode.next;
+            } else {
+                curNode.next = rightNode;
+                rightNode = rightNode.next;
+            }
+            curNode = curNode.next;
+        }
+
+        if(leftNode!=null) {
+            curNode.next = leftNode;
+            leftNode = leftNode.next;
+            curNode = curNode.next;
+        }
+
+        if(rightNode!=null) {
+            curNode.next = rightNode;
+            rightNode = rightNode.next;
+            curNode = curNode.next;
+        }
+
+        return dummyHead.next;
+    }
+
+
+    // public ListNode mergeKLists(ListNode[] lists) {
+    //     // 利用PriorityQueue 来选出最小的一项
+    //     if (lists.length == 0 || lists == null) {
+    //         return null;
+    //     }
+
+    //     ListNode fakehead = new ListNode(0);
+    //     ListNode current = fakehead;
+
+    //     Queue<ListNode> minHeap = new PriorityQueue<ListNode>(lists.length, (node1, node2) -> Integer.compare(node1.val, node2.val));
+    //     for (ListNode listnode : lists) {
+    //         if (listnode != null) {
+    //             minHeap.add(listnode);
+    //         }
+    //     }
+
+    //     while (!minHeap.isEmpty()) {
+    //         ListNode min = minHeap.remove();
+    //         current.next = min;
+    //         current = current.next;
+
+    //         if (min.next != null) {
+    //             minHeap.add(min.next);
+    //         }
+    //     }
+
+    //     return fakehead.next;
+    // }
 }
